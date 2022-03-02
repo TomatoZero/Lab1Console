c70a2e7b00f1b8d36d98ec2f936b3f06d3486711
TomatoZero <chima05.2015@gmail.com>
2022-03-02 19:06:54 +0200

    Revert "test"
    This reverts commit 7a90ead35d467c21631019892fd9783c8f6802b9.

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -7,19 +7,10 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            //Expression expression = new Expression();
-            //while (true) 
-            //{
-            //    Console.WriteLine(expression.CalculateExpression(expression.ConvertToPostfixRecord(Console.ReadLine())));
-            //}
-
-            Equations equations = new Equations();
-
+            Expression expression = new Expression();
             while (true) 
             {
-                equations.AddEquation(Console.ReadLine());
-                double x = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine(equations.FindTheValueOfTheFunction(x));
+                Console.WriteLine(expression.CalculateExpression(expression.ConvertToPostfixRecord(Console.ReadLine())));
             }
 
             /*
